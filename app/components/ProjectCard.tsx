02a91c78d101e4da786f4e76dab863ecbda6e675
Jason Suarez <swarecito@gmail.com>
2023-04-06 16:40:43 +0200

    Setting branch and made project card reusable

---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import React from 'react'
+import Iframe from 'react-iframe';
+import { Inter, Montserrat, Open_Sans, Roboto } from '@next/font/google';
+import Link from 'next/link';
+import { AiOutlineGithub } from 'react-icons/ai';
+
+
+const montserrat = Montserrat({
+  weight: ['100', '300', '400'],
+  subsets: ['latin'],
+});
+
+type AppProps = {
+  url: string;
+  name: string;
+  docs: string
+};
+const ProjectCard = ({url, name, docs }: AppProps) => {
+  return (
+    <div className=''>
+      <Iframe url={url} name={name} height='500px' width='500px'></Iframe>
+
+      <div className='flex justify-center project-caption'>
+        <Link
+          className={clsx(
+            montserrat.className,
+            ' justify-center items-center w-full flex p-5 h-full '
+          )}
+          href={url}
+        >
+          <h1
+            
+          >
+            {name}
+          </h1>
+        </Link>
+        <Link href={docs}>
+          <h1 className={clsx(montserrat.className, ' p-5 ')}>
+            <AiOutlineGithub className='text-2xl' />
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,83 +1,31 @@
 import clsx from 'clsx';
-import React from 'react';
-import { Inter, Montserrat, Open_Sans, Roboto,} from '@next/font/google';
 import Link from 'next/link';
+import ProjectCard from './ProjectCard';
 
-const montserrat = Montserrat({ weight: ['100',"300", "400" ], subsets: ["latin"] });
-const openSans = Open_Sans({
-  weight: [ '300', '400'],
-  subsets: ['latin'],
-});
 const Projects = () => {
   return (
     <section id='projects' className='min-h-[100%]  projects text-center'>
       <h1 className={clsx('text-5xl grid content-center py-20 font-bold')}>
         Projects
       </h1>
-      <div className='flex min-h-[100%] flex-col items-center justify-between lg:flex-row lg:justify-center flex-wrap  px-2'>
-        <div className='p-5  w-[100%] lg:w-[30%] lg:h-96'>
-          <Link
-            className=''
-            target={'_blank'}
-            href={'https://dico-ochre.vercel.app/'}
-          >
-            <div className=''>
-              <img
-                className='rounded w-[100%] rounded-bl-none rounded-br-none'
-                src='./dico-gif.gif'
-                alt='project'
-              />
-            </div>
-            <h1 className={clsx(montserrat.className, 'project-caption py-5 ')}>
-              Dico
-            </h1>
-          </Link>
-        </div>
-        <div className='p-5  w-[100%] lg:w-[30%] lg:h-96'>
-          <Link
-            className=''
-            target={'_blank'}
-            href={'https://gites-mon-tresor-guadeloupe.com/'}
-          >
-            <div className=''>
-              <img
-                className='rounded  w-[100%] rounded-bl-none rounded-br-none'
-                src='./gites-gif.gif'
-                alt='project'
-              />
-            </div>
-            <div className='h-20 '>
-              <h1
-                className={clsx(montserrat.className, 'project-caption py-5 ')}
-              >
-                Gîtes Mon Trésor
-              </h1>
-            </div>
-          </Link>
-        </div>
-        <div className='p-5  w-[100%] lg:w-[30%] lg:h-96'>
-          <Link
-            className=''
-            target={'_blank'}
-            href={'https://dapper-belekoy-aa000e.netlify.app/'}
-          >
-            <div className=''>
-              <img
-                className='rounded w-[100%] max-h-96 rounded-bl-none rounded-br-none'
-                src='./quizoo-gif.gif'
-                alt='project'
-              />
-            </div>
-            <div className='h-20'>
-              <h1
-                className={clsx(montserrat.className, 'project-caption py-5 ')}
-              >
-                QuizoO
-              </h1>
-            </div>
-          </Link>
-        </div>
+      <div className='flex min-h-[100%] flex-col items-center gap-5 justify-between lg:flex-row lg:justify-center flex-wrap  px-2'>
+        <ProjectCard
+          url={'https://gites-mon-tresor-guadeloupe.com/'}
+          name={'Gîtes Mon Trésor'}
+          docs={'https://github.com/All-Khwarizmi/gites-v2'}
+        />
+        <ProjectCard
+          url={'https://codice-it.vercel.app/decks'}
+          name={'Codice'}
+          docs={'https://github.com/All-Khwarizmi/codice'}
+        />
+        <ProjectCard
+          url={'https://dico-uno.vercel.app/'}
+          name={'Dico'}
+          docs={'https://github.com/All-Khwarizmi/Dico'}
+        />
       </div>
+      <div className='p-3'></div>
       <div className='lg:py-10'>
         <Link href='/projects' className=''>
           <button
