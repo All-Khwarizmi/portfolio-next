4b3707661da266d6cdcf8b5542993ed4e3361fe9
Jason Suarez <swarecito@gmail.com>
2024-03-03 19:03:05 +0100

    📦 NEW: Update head description and title

--- app/head.tsx
+++ app/head.tsx
@@ -1,16 +1,16 @@
 export default function Head() {
   return (
     <>
-      <title>Jason's Portfolio</title>
+      <title>Jason Suárez</title>
       <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover'
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
       />
       <meta
-        name='description'
-        content="Jason Suárez || Spanish teacher | Full-stack JavaScript developer | Entrepreneur I create applications to make my student's learning journey smoother."
+        name="description"
+        content="🍎 Professeur d'espagnol et 💻 développeur JavaScript, je fusionne ma passion pour l'éducation avec la technologie pour faciliter le parcours d'apprentissage de mes élèves. Actuellement plongé dans l'univers du Machine Learning et des opérations techniques, je suis engagé dans une quête continue d'apprentissage et d'innovation. Mon objectif est de créer des applications qui non seulement éduquent mais aussi inspirent, en utilisant les dernières avancées technologiques pour répondre aux besoins éducatifs contemporains. "
       />
-      <link rel='icon' href='/portefeuille.png' />
+      <link rel="icon" href="/portefeuille.png" />
     </>
   );
 }
