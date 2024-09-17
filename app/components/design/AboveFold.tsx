cb2a15ec5d8bfcfbcd9f69797d764f4b3be9a633
Jason Suarez <swarecito@gmail.com>
2024-09-17 16:17:14 +0200

    💄 Change the amount of text display on mobile

--- app/components/design/AboveFold.tsx
+++ app/components/design/AboveFold.tsx
@@ -17,7 +17,7 @@ export default function AboveFold() {
     <div className="min-h-screen flex items-center px-4 sm:px-6 lg:px-8 overflow-hidden relative">
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-gray-950 to-transparent" />
       <div className="relative z-10 w-full max-w-7xl mx-auto">
-        <div className="flex flex-col items-center lg:flex-row lg:items-start justify-between gap-12 lg:gap-8">
+        <div className="flex flex-col items-center lg:flex-row lg:items-start justify-between gap-4 lg:gap-8">
           {/* Left Column - Main Headline and CTA Button */}
           <motion.div
             className="w-full lg:w-1/2 space-y-8 text-center lg:text-left"
@@ -25,7 +25,7 @@ export default function AboveFold() {
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold relative">
+            <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold relative">
               <Balancer>
                 <span className="bg-gradient-to-r from-blue-400 to-purple-400 text-transparent bg-clip-text">
                   Donnez vie à vos idées avec des sites web qui captivent
@@ -49,11 +49,17 @@ export default function AboveFold() {
             </h2>
             <p className="text-lg sm:text-xl text-gray-300">
               <Balancer>
-                Imaginez un site web qui s&apos;adapte à tous les écrans, se
-                charge en un clin d&apos;œil, et guide intuitivement vos
-                visiteurs. Des boutons réactifs, des animations fluides, et un
-                back-office aussi simple que votre smartphone. C&apos;est ce
-                que je crée pour vous.
+                <span className="hidden sm:inline">
+                  Imaginez un site web qui s&apos;adapte à tous les écrans, se
+                  charge en un clin d&apos;œil, et guide intuitivement vos
+                  visiteurs. Des boutons réactifs, des animations fluides, et un
+                  back-office aussi simple que votre smartphone. C&apos;est ce
+                  que je crée pour vous.
+                </span>
+                <span className="sm:hidden">
+                  Un site web rapide, intuitif et adaptatif. Des animations fluides 
+                  et un back-office simple. C&apos;est ce que je crée pour vous.
+                </span>
               </Balancer>
             </p>
 
